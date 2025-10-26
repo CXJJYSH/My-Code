714d8d4e079d9f6a08299746b4ec502b9acc3b41
CXJJYSH <1318159468@qq.com>
2025-10-26 12:06:07 +0800

    update

---
+++ 算法/力扣/灵神/讲解/基础算法精讲/滑动窗口/作业/2962. 统计最大元素出现至少 K 次的子数组.py
@@ -0,0 +1,53 @@
+# “越长越合法”型滑动窗口
+
+class Solution:
+    def countSubarrays(self, nums: list[int], k: int) -> int:
+        
+        # 我的代码超出时间限制了😢。写了二十多分钟，快三十分钟了。
+        
+        ans = 0
+        max_value = max(nums)
+        num = 0
+        for right, i in enumerate(nums):
+            left = 0
+            if i == max_value:
+                num += 1
+            cnt = num
+            while cnt >= k:
+                ans += 1
+                if nums[left] == max_value:
+                    cnt -= 1
+                left += 1
+        return ans
+        
+        # 这是自己看了灵神的题解后自己写的代码。超过了74.02%。
+        
+        mx = max(nums)
+        ans = 0
+        cnt = 0
+        left = 0
+        for right, i in enumerate(nums):
+            if i == mx:
+                cnt += 1
+            while cnt == k:
+                if nums[left] == mx:
+                    cnt -= 1
+                left += 1
+            ans += left
+        return ans
+        
+        # 这是灵神的代码。超过了93.48%。
+        
+        mx = max(nums)
+        ans = 0
+        cnt = 0
+        left = 0
+        for i in nums:
+            if i == mx:
+                cnt += 1
+            while cnt == k:
+                if nums[left] == mx:
+                    cnt -= 1
+                left += 1
+            ans += left # 灵神的代码太优雅了，竟然可以通过这样的一个巧妙计算算出正确答案。越长越合法型滑动窗口题原来可以这样做。
+        return ans
