e1a3ba52302e0053312388396384760796dce138
CXJJYSH <1318159468@qq.com>
2025-11-06 21:14:38 +0800

    update

--- 算法/力扣/灵神/讲解/基础算法精讲/二分查找/作业/简单/2529. 正整数和负整数的最大计数.py
+++ 算法/力扣/灵神/讲解/基础算法精讲/二分查找/作业/简单/2529. 正整数和负整数的最大计数.py
@@ -37,7 +37,7 @@ class Solution:
         # 灵神的简洁二分查找代码。
         # 学到了之前没见过的新函数bisect_left(a, b)和bisect_right(a, b)。
         # 使用前提是有序数组。（这道题为非递减数组）
-        # 作用：前者返回a中第一个等于b的元素的索引，后者返回a中第一个大于b的元素的索引。
+        # 作用：前者返回a中第一个等于b的元素的索引，后者返回a中第一个大于b的元素的索引。 
 
         neg = bisect_left(nums, 0)
         pos = len(nums) - bisect_right(nums, 0)
