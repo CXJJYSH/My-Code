db46620798a3f01e1c20703e45d7240e41eafb25
CXJJYSH <1318159468@qq.com>
2025-11-10 12:52:01 +0800

    update

--- 算法/力扣/灵神/讲解/基础算法精讲/03 二分查找/作业/中等/2300. 咒语和药水的成功对数.py
+++ 算法/力扣/灵神/讲解/基础算法精讲/03 二分查找/作业/中等/2300. 咒语和药水的成功对数.py
@@ -37,5 +37,26 @@ class Solution:
         success -= 1
         return [m - bisect_right(potions, success // x) for x in spells]
     
-# 还有方法二 计数 + 值域前缀和 写法，但是今天先不看这部分题解，等我把前缀和那道题做完之后再来写方法二的代码。
+# 还有方法二 计数 + 值域后缀和 写法，但是今天先不看这部分题解，等我把前缀和那道题做完之后再来写方法二的代码。
 # 2025.11.07 13:14
+
+class Solution:
+    def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
+
+        mx = max(potions)
+        cnt = [0] * (mx + 1) # 初始化计数数组，包含0，0和1到mx总共有(mx + 1)个数，所以初始化数组长度为(mx + 1)。
+        
+        for y in potions:
+            cnt[y] += 1 # 统计每种药水的出现次数
+        
+        # 计算 cnt 的后缀和
+        for i in range(mx - 1, -1 , -1):
+            cnt[i] += cnt[i + 1] 
+        # 这里是从数组的倒数第二个元素开始从后往前遍历的，因为最后一个元素不用更新，只需要从倒数第二个元素开始更新。
+        # 计算完毕后，cnt[i] 就是 potions 值 >= i 的药水个数
+        
+        for i, x in enumerate(spells):
+            low = (success - 1) // x + 1
+            spells[i] = cnt[low] if low <= mx else 0 
+        # 这里是查找满足条件的最小元素是否在potions里，如果在(low <= mx)则返回cnt[low]，如果不在(不满足low <= mx)则返回0。
+        return spells # 这是原地改变spells数组然后返回spells。且一次改变一个spells的元素不会影响最后答案的正确性。 
