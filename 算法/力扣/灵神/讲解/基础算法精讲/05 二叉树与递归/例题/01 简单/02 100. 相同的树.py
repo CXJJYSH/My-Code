9c52d534a63dd9255e672fe5dbb1f9281056392e
CXJJYSH <1318159468@qq.com>
2025-11-20 15:32:55 +0800

    update

---
+++ 算法/力扣/灵神/讲解/基础算法精讲/05 二叉树与递归/例题/01 简单/02 100. 相同的树.py
@@ -0,0 +1,26 @@
+from typing import Optional
+
+# 单向链表的定义。
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+# 下面的灵神方法的代码，优雅，太优雅了。
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if p is None or q is None:
+            return p is q
+        # 如果有节点为空，就看是不是都为空。
+
+        return (p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right))
+        # 若都不为空，则看节点值是否相等、两节点的左子树是否相等、两节点的右子树是否相等。 
+
+# 时间复杂度O(n)
+# 空间复杂度O(n)
+
+# 简单
+# 2025.11.20 13:36
---
+++ 算法/力扣/灵神/讲解/基础算法精讲/05 二叉树与递归/例题/01 简单/03 101. 对称二叉树.py
@@ -0,0 +1,25 @@
+from typing import Optional
+
+# 单向链表的定义。
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    
+    def isSymmetricTree(self, p, q):
+        # 判断是否是对称树。
+
+        if p is None or q is None:
+            return p is q
+        # 依旧要检查当前两节点是否为空。
+
+        return (p.val == q.val and self.isSymmetricTree(p.left, q.right) and self.isSymmetricTree(p.right, q.left))
+        # 因为是判断是否为对称树，所以要左对右、右对左。
+
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        return self.isSymmetricTree(root.left, root.right)
+        # 最后因为题目保证了至少有一个节点，所以不用判断根节点是否为空，直接对左右子树进行判断是否对称就可以了。 
