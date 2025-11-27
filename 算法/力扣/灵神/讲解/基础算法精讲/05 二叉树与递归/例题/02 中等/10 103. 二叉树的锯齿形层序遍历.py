3aed84f40945366f3dc38df93a214a48b53fb4cc
CXJJYSH <1318159468@qq.com>
2025-11-27 23:42:24 +0800

    update

---
+++ 算法/力扣/灵神/讲解/基础算法精讲/05 二叉树与递归/例题/02 中等/10 103. 二叉树的锯齿形层序遍历.py
@@ -0,0 +1,56 @@
+from collections import deque
+from typing import List, Optional
+
+# 二叉树定义。
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if root is None:
+            return []
+        ans = []
+        q = deque([root]) # 用双端队列写。
+        even = False
+        while q:
+            vals = []
+            for _ in range(len(q)):
+                node = q.popleft()
+                vals.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            ans.append(vals[::-1] if even else vals)
+            even = not even 
+        return ans 
+        # 也可以用两个cur, nxt数组来写。 
+
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if root is None:
+            return []
+        ans = []
+        cur = [root]
+        even = False 
+        while cur:
+            nxt = []
+            vals = []
+            for node in cur:
+                vals.append(node.val)
+                if node.left:
+                    nxt.append(node.left)
+                if node.right:
+                    nxt.append(node.right)
+            ans.append(vals[::-1] if even else vals)
+            cur = nxt 
+            even = not even 
+        return ans 
+    
+# 时间复杂度O(n)
+# 空间复杂度O(n)
+# 2025.11.27 23:42
