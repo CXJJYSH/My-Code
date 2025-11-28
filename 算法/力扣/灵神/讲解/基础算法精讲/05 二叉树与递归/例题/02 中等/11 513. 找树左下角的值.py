c9c9441c2b0a857fdc122279187262907127a013
CXJJYSH <1318159468@qq.com>
2025-11-28 12:58:42 +0800

    update

--- 算法/力扣/灵神/讲解/基础算法精讲/05 二叉树与递归/例题/02 中等/11 513. 找树左下角的值.py
+++ 算法/力扣/灵神/讲解/基础算法精讲/05 二叉树与递归/例题/02 中等/11 513. 找树左下角的值.py
@@ -13,13 +13,24 @@ class Solution:
     def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
         if root is None:
             return []
+        # 特判空节点。 
+        # 题目说了至少有一个节点，所以其实这道题不用特判根节点是否为空。 
+        # 直接开始初始化双端队列就好。 
+        
         ans = root.val 
+        # 把ans = root.val这行删掉也可以。 
+        # 因为前面已经特判空节点了，后面若能进入循环,vals.append(node.val)和ans = vals[0]语句的结果就一定是正确的， 
+        # 所以就可以将ans放到后面来赋值。
+
         q = deque([root])
         while q:
             vals = []
             # 遍历每一行的时候先初始化记录数组。
 
             for _ in range(len(q)):
+                # 这里我用了vals数组记录每一行的节点值的话，就一定要再用一层for循环， 
+                # 不然就是q每跳出一个节点，vals就置为空数组，去记录下一“个”节点值了，最后返回的会是最后一行最后一个节点的节点值。 
+                
                 node = q.popleft()
                 vals.append(node.val)
                 if node.left:
@@ -39,3 +50,28 @@ class Solution:
 
 # 中等
 # 2025.11.28 12:31 从十二点出头开始做的，大概花了半个小时写代码和做笔记。 
+
+# 灵神的代码还是依旧优雅。
+
+class Solution:
+    def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
+        q = deque([root])
+        # 不用特判，直接初始化双端队列。
+
+        while q:
+            node = q.popleft()
+            
+            if node.right:
+                q.append(node.right)
+            if node.left:
+                q.append(node.left)
+            # 调整if语句的判断顺序就能起到调整节点进入双端队列的顺序的效果。
+
+        return node.val 
+        # 返回最后记录的node的节点值即可。 
+
+# 灵神的代码在力扣上测试比我快的原因是———— 
+# 他代码的常数操作更少，消耗更少的常数时间，即他少了我额外创建数组所消耗的时间， 
+# 他的代码常数开销更小，速度略快。 
+
+# 2025.11.28 12:58 
