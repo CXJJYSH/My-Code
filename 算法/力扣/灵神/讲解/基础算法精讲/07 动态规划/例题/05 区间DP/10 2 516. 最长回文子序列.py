a69de404ac52945afbcf5e838e854c5038eb33cf
CXJJYSH <1318159468@qq.com>
2025-12-19 18:07:13 +0800

    update

--- 算法/力扣/灵神/讲解/基础算法精讲/07 动态规划/例题/05 区间DP/10 2 516. 最长回文子序列.py
+++ 算法/力扣/灵神/讲解/基础算法精讲/07 动态规划/例题/05 区间DP/10 2 516. 最长回文子序列.py
@@ -27,3 +27,87 @@ class Solution:
 # 空间复杂度O(n ^ 2) 
 
 # 2025.12.19 17:37 
+
+# 递推写法 
+
+class Solution:
+    def longestPalindromeSubseq(self, s: str) -> int:
+        n = len(s)
+        
+        f = [[0] * n for _ in range(n)]
+        
+        for i in range(n - 1, -1, -1):
+            # 因为要从i + 1的情况转移到i，要先把i + 1算出来，所以到倒序遍历i。 
+
+            f[i][i] = 1
+            
+            for j in range(i + 1, n):
+                # 因为要从j - 1的情况转移到j，要先把j - 1算出来，所以要正序遍历j。 
+
+                if s[i] == s[j]:
+                    f[i][j] = f[i + 1][j - 1] + 2
+                else:
+                    f[i][j] = max(f[i + 1][j], f[i][j - 1])
+        
+        return f[0][n - 1] 
+    
+# 时间复杂度O(n ^ 2) 
+# 空间复杂度O(n ^ 2) 
+
+# 2025.12.19 17:50 
+
+# 递推写法空间优化
+
+class Solution:
+    def longestPalindromeSubseq(self, s: str) -> int:
+        n = len(s)
+        
+        f = [0] * n
+        
+        for i in range(n - 1, -1, -1):
+            # 倒序遍历的原因同上。 
+
+            f[i] = 1
+            pre = 0 # 初始值相当于f[i + 1][i]。 
+            
+            for j in range(i + 1, n):
+                # 正序遍历的原因同上。 
+
+                tmp = f[j] 
+                f[j] = pre + 2 if s[i] == s[j] else max(f[j], f[j - 1])
+                pre = tmp 
+                # 这里因为f[j]对应的状态可能是f[i][j]、也可能是f[i + 1][j]，要准确地保存新旧数据，所以要用到中间变量暂存。  
+
+        return f[-1] # 最后返回数组最后一个元素。 
+    
+# 时间复杂度O(n ^ 2) 
+# 空间复杂度O(n) 
+
+# 2025.12.19 18:03 
+
+# 递推写法空间优化中用并行赋值也可以。 
+
+class Solution:
+    def longestPalindromeSubseq(self, s: str) -> int:
+        n = len(s)
+        
+        f = [0] * n
+        
+        for i in range(n - 1, -1, -1):
+            # 倒序遍历的原因同上。 
+
+            f[i] = 1
+            pre = 0 # 初始值相当于f[i + 1][i]。 
+
+            for j in range(i + 1, n):
+                # 正序遍历的原因同上。 
+
+                f[j], pre= pre + 2 if s[i] == s[j] else max(f[j], f[j - 1]), f[j]
+                # 并行赋值也没错。 
+
+        return f[-1] # 最后返回数组最后一个元素。 
+    
+# 时间复杂度O(n ^ 2) 
+# 空间复杂度O(n) 
+
+# 2025.12.19 18:05 
