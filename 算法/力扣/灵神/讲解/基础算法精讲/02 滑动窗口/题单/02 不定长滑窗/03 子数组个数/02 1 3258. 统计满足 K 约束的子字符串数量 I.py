b405bd2882c3babb8eeda99883c0bb185d4dd9f5
CXJJYSH <1318159468@qq.com>
2026-01-25 00:12:01 +0800

    update

--- 算法/力扣/灵神/讲解/基础算法精讲/02 滑动窗口/题单/02 不定长滑窗/03 子数组个数/02 1 3258. 统计满足 K 约束的子字符串数量 I.py
+++ 算法/力扣/灵神/讲解/基础算法精讲/02 滑动窗口/题单/02 不定长滑窗/03 子数组个数/02 1 3258. 统计满足 K 约束的子字符串数量 I.py
@@ -1,3 +1,5 @@
+# 我的写法————条件判断。 
+
 class Solution:
     def countKConstraintSubstrings(self, s: str, k: int) -> int:
         ans = 0
@@ -28,3 +30,51 @@ class Solution:
 # 空间复杂度O(1) 
 
 # 2026.01.24 23:58 
+
+# 灵神的写法————ASCII码与1（&1）快速取末位。 
+
+class Solution:
+    def countKConstraintSubstrings(self, s: str, k: int) -> int:
+        ans = 0
+        cnt = [0, 0]
+        left = 0
+
+        for right, x in enumerate(s):
+            cnt[ord(x) & 1] += 1
+            
+            while cnt[0] > k and cnt[1] > k:
+                cnt[ord(s[left]) & 1] -= 1
+                left += 1
+            
+            ans += right - left + 1
+        
+        return ans 
+    
+# 时间复杂度O(n) 
+# 空间复杂度O(1) 
+
+# 2026.01.25 00:07 
+
+# 也可以模2（%2）快速取末位。 
+
+class Solution:
+    def countKConstraintSubstrings(self, s: str, k: int) -> int:
+        ans = 0
+        cnt = [0, 0]
+        left = 0
+
+        for right, x in enumerate(s):
+            cnt[ord(x) % 2] += 1
+            
+            while cnt[0] > k and cnt[1] > k:
+                cnt[ord(s[left]) % 2] -= 1
+                left += 1
+            
+            ans += right - left + 1
+        
+        return ans 
+
+# 时间复杂度O(n) 
+# 空间复杂度O(1) 
+
+# 2026.01.25 00:11 
