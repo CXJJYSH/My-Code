9a6fed81bd6c8ac068528ca4a254b36f8625fe01
CXJJYSH <1318159468@qq.com>
2026-01-26 17:56:40 +0800

    update

--- 算法/力扣/灵神/讲解/基础算法精讲/02 滑动窗口/题单/02 不定长滑窗/03 子数组个数/04 2 2762. 不间断子数组.py
+++ 算法/力扣/灵神/讲解/基础算法精讲/02 滑动窗口/题单/02 不定长滑窗/03 子数组个数/04 2 2762. 不间断子数组.py
@@ -1,4 +1,7 @@
+from collections import deque
 from typing import Counter, List
+
+# 滑动窗口 + 集合/哈希表 写法 
 
 class Solution:
     def continuousSubarrays(self, nums: List[int]) -> int:
@@ -29,3 +32,49 @@ class Solution:
 # 空间复杂度O(D) / 空间复杂度O(D + 1) 
 
 # 2026.01.25 11:54 
+
+# 滑动窗口 + 单调队列 写法 
+
+class Solution:
+    def continuousSubarrays(self, nums: List[int]) -> int:
+        min_q = deque()
+        max_q = deque()
+        # 这里的deque()是创建双端队列的意思。 
+        # 这里的双端队列保存的是元素索引。 
+        # deque: double-ended queue 
+        
+        ans = 0
+        left = 0
+        
+        for right, x in enumerate(nums):
+            
+            while min_q and x <= nums[min_q[-1]]:
+                min_q.pop()
+            min_q.append(right)
+            # 将最小值队列更新成从最小值开始从小到大排列。 
+
+            while max_q and x >= nums[max_q[-1]]:
+                max_q.pop()
+            max_q.append(right)
+            # 将最大值队列更新成从最大值开始从大到小排列。 
+            
+            while nums[max_q[0]] - nums[min_q[0]] > 2:
+                left += 1
+                # 先滑动窗口，缩小窗口大小。 
+                if min_q[0] < left:
+                    min_q.popleft()
+                # 如果最小值出去了，删除最小值的索引。 
+
+                if max_q[0] < left:
+                    max_q.popleft()
+                # 如果最大值出去了，删除最大值的索引。 
+
+            ans += right - left + 1
+            # 更新答案。 
+
+        return ans 
+    
+# 时间复杂度O(n)，这种写法就没有max()和min()这两个内置函数增加时间复杂度了。 
+# 空间复杂度O(n) 
+
+# 2026.01.26 17:56 
