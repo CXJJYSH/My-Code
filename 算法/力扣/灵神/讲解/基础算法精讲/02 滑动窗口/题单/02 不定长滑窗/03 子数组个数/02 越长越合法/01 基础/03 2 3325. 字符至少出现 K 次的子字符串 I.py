f9e87fab16e8330973ed275c9a047e83708b7537
CXJJYSH <1318159468@qq.com>
2026-01-30 14:39:14 +0800

    update

---
+++ 算法/力扣/灵神/讲解/基础算法精讲/02 滑动窗口/题单/02 不定长滑窗/03 子数组个数/02 越长越合法/01 基础/03 2 3325. 字符至少出现 K 次的子字符串 I.py
@@ -0,0 +1,32 @@
+from collections import defaultdict
+
+class Solution:
+    def numberOfSubstrings(self, s: str, k: int) -> int:
+        ans = 0
+        cnt= defaultdict(int)
+        left = 0
+
+        for c in s:
+            cnt[c] += 1
+            
+            while cnt[c] >= k:
+                cnt[s[left]] -= 1
+                left += 1
+            
+            ans += left 
+        
+        return ans 
+    
+# 什么时候需要删值为0的键： 
+# 看cnt容器本身时需要删， 看cnt[x]值时不需要删。 
+
+# 这里用 cnt[c] >= k 作为while循环条件可行的原因是，只有上一步 cnt[c] += 1 会触发while循环， 
+# 且只有当前c元素键的值达到了k，其它必然小于k、无需判断， 
+# 而事实上是cnt[c]刚等于k的时候就触发while循环了，所以while循环条件也可以写为 cnt[c] == k 。 
+
+# 灵神这样写时间复杂度和空间复杂度就很低了。 
+
+# 时间复杂度O(n) 
+# 空间复杂度O(U)，U为字符集合的大小。 
+
+# 2026.01.30 14:37 
