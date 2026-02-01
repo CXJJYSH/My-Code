70f7e7bc31cb8bb7167616e040ce5366496c448e
CXJJYSH <1318159468@qq.com>
2026-02-01 20:05:02 +0800

    update

---
+++ 算法/力扣/灵神/讲解/基础算法精讲/02 滑动窗口/题单/02 不定长滑窗/03 子数组个数/02 越长越合法/01 基础/05 2 2537. 统计好子数组的数目.py
@@ -0,0 +1,35 @@
+from collections import defaultdict
+from typing import List
+
+class Solution:
+    def countGood(self, nums: List[int], k: int) -> int:
+        ans = 0
+        cnt = defaultdict(int)
+        pairs = 0
+        left = 0
+
+        for x in nums:
+            pairs += cnt[x] 
+
+            cnt[x] += 1 
+
+            while pairs >= k:
+                out = nums[left] 
+                cnt[out] -= 1
+
+                pairs -= cnt[out] 
+
+                left += 1
+
+            ans += left 
+
+        return ans 
+    
+# 这道题的奇特之处在于加数对数和减数对数时的数量关系。 
+# 进x的时候，进之前有cnt[x]个x，进之后就会增加cnt[x]个数对，所以pairs要先加cnt[x]个，然后再将cnt[x]加1。 
+# 出x的时候，出之前有cnt[x]个x，出之后就会减少 cnt[x] - 1 个数对，所以先将当前cnt[x]减1，再将pairs加减1后的cnt[x]个。 
+
+# 时间复杂度O(n) 
+# 空间复杂度O(n) 
+
+# 2026.02.01 20:04 
