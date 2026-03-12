407d72babeaf04eaf3689f64181fd9861935dc5b
CXJJYSH <1318159468@qq.com>
2026-03-12 23:07:47 +0800

    update

--- 算法/力扣/灵神/讲解/基础算法精讲/02 双指针/03 题单/01 单序列双指针/01 反转字符串/01 基础/12 1 345. 反转字符串中的元音字母.py
+++ 算法/力扣/灵神/讲解/基础算法精讲/02 双指针/03 题单/01 单序列双指针/01 反转字符串/01 基础/12 1 345. 反转字符串中的元音字母.py
@@ -22,3 +22,29 @@ class Solution:
 # 空间复杂度O(n) 
 
 # 2026.03.12 22:14 
+
+# 我的另一份写法，这一份逻辑结构更简单。 
+
+class Solution:
+    def reverseVowels(self, s: str) -> str:
+        s = list(s) 
+
+        left = 0
+        right = len(s) - 1
+
+        while left < right:
+            while not s[left] in "aeiouAEIOU" and left < right:
+                left += 1
+            while not s[right] in "aeiouAEIOU" and left < right:
+                right -= 1
+
+            s[left], s[right] = s[right], s[left] 
+            left += 1
+            right -= 1
+
+        return "".join(s) 
+    
+# 时间复杂度O(n) 
+# 空间复杂度O(n) 
+
+# 2026.03.12 23:07 
