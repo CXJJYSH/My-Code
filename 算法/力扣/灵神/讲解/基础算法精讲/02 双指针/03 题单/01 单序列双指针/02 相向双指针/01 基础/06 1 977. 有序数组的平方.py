c6179d37646792ffe906d8db6bb5ce073bc50209
CXJJYSH <1318159468@qq.com>
2026-03-16 23:38:00 +0800

    update

---
+++ 算法/力扣/灵神/讲解/基础算法精讲/02 双指针/03 题单/01 单序列双指针/02 相向双指针/01 基础/06 1 977. 有序数组的平方.py
@@ -0,0 +1,84 @@
+from typing import List
+
+# 看了灵神的题解后我写出来的代码。 
+
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        ans = [0] * len(nums) 
+        
+        left = 0
+        right = len(nums) - 1
+        
+        p = len(nums) - 1
+        
+        while p >= 0:
+            x = nums[left] ** 2
+            y = nums[right] ** 2
+        
+            if x > y:
+                ans[p] = x
+                p -= 1
+                left += 1
+            else:
+                ans[p] = y
+                p -= 1
+                right -= 1
+        
+        return ans 
+    
+# 时间复杂度O(n) 
+# 空间复杂度O(1) 
+
+# 2026.03.16 23:32 
+
+# 灵神的写法，用的for循环，我用的while循环， 
+
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        ans = [0] * len(nums) 
+        
+        left = 0
+        right = len(nums) - 1
+        p = len(nums) - 1
+        
+        for p in range(len(nums) - 1, -1, -1):
+            x = nums[left] ** 2
+            y = nums[right] ** 2
+        
+            if x > y:
+                ans[p] = x
+                left += 1
+            else:
+                ans[p] = y
+                right -= 1
+        
+        return ans 
+    
+# 灵神的另一种写法，只用计算一次乘法。 
+
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        ans = [0] * len(nums) 
+        
+        left = 0
+        right = len(nums) - 1
+        
+        p = len(nums) - 1
+        
+        for p in range(len(nums) - 1, -1, -1):
+            x = nums[left]
+            y = nums[right]
+        
+            if -x > y:
+                ans[p] = x * x
+                left += 1
+            else:
+                ans[p] = y * y
+                right -= 1
+        
+        return ans 
+        
+# 时间复杂度O(n) 
+# 空间复杂度O(1) 
+
+# 2026.03.16 23:37 
