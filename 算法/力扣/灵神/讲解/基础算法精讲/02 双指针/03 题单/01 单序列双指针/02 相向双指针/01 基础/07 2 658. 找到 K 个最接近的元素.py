d1da7040b11f5cd0c41de527ab23eb44862e3210
CXJJYSH <1318159468@qq.com>
2026-03-17 11:14:25 +0800

    update

---
+++ 算法/力扣/灵神/讲解/基础算法精讲/02 双指针/03 题单/01 单序列双指针/02 相向双指针/01 基础/07 2 658. 找到 K 个最接近的元素.py
@@ -0,0 +1,26 @@
+from typing import List
+
+# 我的写法 
+
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        left = 0
+        right = len(arr) - 1
+
+        while right - left + 1 > k:
+            if abs(arr[left] - x) < abs(arr[right] - x):
+                right -= 1
+            elif abs(arr[left] - x) == abs(arr[right] - x) and arr[left] < arr[right]:
+                right -= 1
+            # 根据题目条件可知只要不满足上述两种情况的就是b比a更接近的情况，这时移动左指针即可。 
+            else:
+                left += 1
+        
+        return arr[left : right + 1]
+
+# 时间复杂度O(n) 
+# 空间复杂度O(1) 
+
+# 2026.03.17 11:07 ~ 11:11 四分钟搞定。 
+
+# 2026.03.17 11:12 
