ed7c3cd40bd1172b31dbaac7b7f72d22c98ca2dd
CXJJYSH <1318159468@qq.com>
2026-03-19 10:45:49 +0800

    update

--- 算法/力扣/灵神/讲解/基础算法精讲/02 双指针/03 题单/01 单序列双指针/02 相向双指针/01 基础/09 2 167. 两数之和 II - 输入有序数组.py
+++ 算法/力扣/灵神/讲解/基础算法精讲/02 双指针/03 题单/01 单序列双指针/02 相向双指针/01 基础/09 2 167. 两数之和 II - 输入有序数组.py
@@ -1,5 +1,9 @@
 from typing import List
 
+# 灵神解释的本题要点 
+# ⚠注意： 
+# 虽然题目说下标从 1 开始，但输入的 numbers 数组的下标仍然是从 0 开始的。我们应当按照从 0 开始的下标写代码，只是在最后返回的时候，把下标加一。
+
 # 之前我写的代码。 
 
 # 2025.10.14 
@@ -46,3 +50,27 @@ class Solution:
 # 空间复杂度O(1) 
 
 # 2026.03.19 10:20 
+
+# 灵神的写法 
+
+# 时间击败率都是一样的。灵神的还是更简洁一点。 
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left = 0
+        right = len(numbers) - 1
+
+        while True:
+            s = numbers[left] + numbers[right] 
+        
+            if s == target:
+                return [left + 1, right + 1]
+            elif s > target:
+                right -= 1
+            else: 
+                left += 1
+
+# 时间复杂度O(n) 
+# 空间复杂度O(1) 
+
+# 2026.03.19 10:45 
