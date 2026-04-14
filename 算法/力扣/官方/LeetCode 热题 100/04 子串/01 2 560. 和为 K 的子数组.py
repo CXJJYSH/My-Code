5c1422c25bc41e9019eed946e18667544c911a7d
CXJJYSH <1318159468@qq.com>
2026-04-14 12:55:01 +0800

    update

---
+++ 算法/力扣/官方/LeetCode 热题 100/04 子串/01 2 560. 和为 K 的子数组.py
@@ -0,0 +1,35 @@
+from collections import defaultdict
+from typing import List
+
+# 2026.02.03 前缀和写法 
+
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        ans = 0
+        cnt = defaultdict(int) 
+        s = 0
+        for x in nums:
+            cnt[s] += 1
+            s += x 
+            ans += cnt[s - k]
+        return ans 
+    
+# 2026.04.14 12:48 前缀和写法 
+
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        ans = 0
+        s = 0 # 前缀和 
+        cnt = defaultdict(int) # 不同前缀和出现次数的统计字典 
+        
+        for x in nums:
+            cnt[s] += 1 # 当前元素之前的元素和-即前缀和-数量加一 
+            s += x # 更新为后一个元素的前缀和 
+            ans += cnt[s - k] # 不知道为什么要在这里计算答案 
+        
+        return ans 
+
+# 时间复杂度O(n)，nums长度。 
+# 空间复杂度O(m)，字典长度。 
+
+# 2026.04.14 12:54 
