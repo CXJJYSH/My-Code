aa95c291ad949aed5c6bd620c7635459aad84d23
CXJJYSH <1318159468@qq.com>
2026-05-18 12:05:25 +0800

    update

---
+++ 算法/力扣/官方/LeetCode 热题 100/11 二分查找/067 2 153. 寻找旋转排序数组中的最小值.py
@@ -0,0 +1,65 @@
+from typing import List
+
+# 2025.11.03 
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        
+        # 依旧可以只遍历第一个元素到倒数第二个元素。
+        
+        # 左开右开
+        '''
+        left = -1
+        right = len(nums) - 1
+        while left + 1 < right:
+            mid = (left + right) // 2
+            if nums[mid] < nums[-1]:
+                right = mid # 向最小值逼近。
+            else:
+                left = mid # 向最小值逼近。
+        return nums[right] # 左开右开写法返回右指针。
+        '''
+        # 左闭右开
+        '''
+        left = 0
+        right = len(nums) - 1
+        while left < right:
+            mid = (left + right) // 2
+            if nums[mid] < nums[-1]:
+                right = mid # 向最小值逼近。
+            else:
+                left = mid + 1 # 向最小值逼近。
+        return nums[left] # 左闭右开写法返回左右指针都行。
+        '''
+        # 左闭右闭
+
+        left = 0
+        right = len(nums) - 2
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] < nums[-1]:
+                right = mid - 1 # 向最小值逼近。
+            else:
+                left = mid + 1 # 向最小值逼近。
+        return nums[left] # 左闭右闭写法返回左指针。 
+    
+# 2026.05.18 12:04 
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        left, right = -1, len(nums) - 1
+
+        while left + 1 < right:
+            mid = (left + right) // 2
+        
+            if nums[mid] < nums[-1]:
+                right = mid 
+            else:
+                left = mid 
+        
+        return nums[right]
+    
+# 时间复杂度O(log n) 
+# 空间复杂度O(1) 
+
+# 2026.05.18 12:05 
