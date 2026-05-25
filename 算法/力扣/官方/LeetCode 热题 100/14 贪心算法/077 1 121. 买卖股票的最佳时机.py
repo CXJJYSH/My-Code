261fcca0bd1110816677c1df67c789c1d528755a
CXJJYSH <1318159468@qq.com>
2026-05-25 11:12:32 +0800

    update

--- 算法/力扣/官方/LeetCode 热题 100/14 贪心算法/077 1 121. 买卖股票的最佳时机.py
+++ 算法/力扣/官方/LeetCode 热题 100/14 贪心算法/077 1 121. 买卖股票的最佳时机.py
@@ -9,3 +9,32 @@
 # 今天又是啥也没干，去玩了一天。 
 
 # 2026.05.24 23:25 
+
+from typing import List
+
+# 2026.04.16 
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        ans = 0
+        min_price = prices[0]
+        for p in prices: 
+            ans = max(ans, p - min_price)
+            min_price = min(min_price, p)
+        return ans 
+    
+# 2026.05.25 11:09 
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        ans = 0
+        min_price = prices[0]
+        for i in prices:
+            ans = max(ans, i - min_price)
+            min_price = min(i, min_price)
+        return ans 
+
+# 时间复杂度O(n) 
+# 空间复杂度O(1) 
+
+# 2026.05.25 11:11 
