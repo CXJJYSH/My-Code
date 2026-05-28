ea73ac325e85438c058377a959b7d478c4336ee7
CXJJYSH <1318159468@qq.com>
2026-05-28 18:32:42 +0800

    update

--- 教程/01_SuperSimpleDev/03_JavaScript/11_Arrays_and_Loops/01_copy_of_code/scripts/11_todo_list.js
+++ 教程/01_SuperSimpleDev/03_JavaScript/11_Arrays_and_Loops/01_copy_of_code/scripts/11_todo_list.js
@@ -1,4 +1,13 @@
-const todoList = ["make dinner", "wash dishes"];
+const todoList = [
+  {
+    name: "make dinner",
+    dueDate: "2022-12-22",
+  },
+  {
+    name: "wash dishes",
+    dueDate: "2022-12-22",
+  },
+];
 
 renderTodoList();
 
@@ -6,12 +15,33 @@ function renderTodoList() {
   let todoListHTML = "";
 
   for (let i = 0; i < todoList.length; i++) {
-    const todo = todoList[i];
-    const html = `<p>${todo}</p>`;
+    const todoObject = todoList[i];
+
+    // const name = todoObject.name;
+    // const dueDate = todoObject.dueDate;
+    const { name, dueDate } = todoObject;
+
+    const html = `
+      <div>
+        ${name}
+      </div>
+
+      <div>
+        ${dueDate}
+      </div>
+
+      <button class="delete-todo-button" onclick="
+        todoList.splice(${i}, 1);
+        renderTodoList();
+      ">
+        Delete
+      </button>
+    `; // 这里每次删完一个都会重新运行一遍显示待办的函数，重新给待办排序，所以保证了删除按钮的可行性和正确性。
+
     todoListHTML += html;
   }
 
-  console.log(todoListHTML);
+  // console.log(todoListHTML);
 
   document.querySelector(".js-todo-list").innerHTML = todoListHTML;
 }
@@ -20,8 +50,17 @@ function addTodo() {
   const inputElement = document.querySelector(".js-name-input");
   const name = inputElement.value;
 
-  todoList.push(name);
-  console.log(todoList);
+  const dateInputElement = document.querySelector(".js-due-date-input");
+  const dueDate = dateInputElement.value;
+
+  todoList.push({
+    // name: name,
+    // dueDate: dueDate,
+    name,
+    dueDate,
+  });
+
+  // console.log(todoList);
 
   inputElement.value = "";
 
@@ -31,3 +70,5 @@ function addTodo() {
 // 2026.05.26 16:09
 
 // 2026.05.26 17:26
+
+// 2026.05.28 18:04
