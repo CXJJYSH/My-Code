b33dd8b9779fb8c9c1cc642d8a841403a52c65df
CXJJYSH <1318159468@qq.com>
2026-08-04 16:27:23 +0800

    Make the cart quantity interactive

--- 01_Tutorial/01_SuperSimpleDev/03_JavaScript/13_Start_the_Amazon_Project_and_Intro_to_Git/01_copy_of_code/javascript-amazon-project/javascript-amazon-project-main/scripts/amazon.js
+++ 01_Tutorial/01_SuperSimpleDev/03_JavaScript/13_Start_the_Amazon_Project_and_Intro_to_Git/01_copy_of_code/javascript-amazon-project/javascript-amazon-project-main/scripts/amazon.js
@@ -72,10 +72,19 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
         quantity: 1,
       });
     }
-    console.log(cart);
+
+    let cartQuantity = 0;
+
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+
+    document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
   });
 });
 
 // 2026.08.04 15:03
 
 // 2026.08.04 15:32
+
+// 2026.08.04 16:26
