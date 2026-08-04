94531840ffed20596a5f9aef95003a40db3287c0
CXJJYSH <1318159468@qq.com>
2026-08-04 23:39:49 +0800

    update

--- 01_Tutorial/01_SuperSimpleDev/03_JavaScript/13_Start_the_Amazon_Project_and_Intro_to_Git/02_exercises/scripts/amazon.js
+++ 01_Tutorial/01_SuperSimpleDev/03_JavaScript/13_Start_the_Amazon_Project_and_Intro_to_Git/02_exercises/scripts/amazon.js
@@ -23,7 +23,7 @@ products.forEach((product) => {
         </div>
 
         <div class="product-quantity-container">
-          <select>
+          <select class="js-quantity-selector-${product.id}">
             <option selected value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
@@ -63,6 +63,12 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
         matchingItem = item;
       }
     });
+
+    const quantitySelector = document.querySelector(
+      `.js-quantity-selector-${productId}`,
+    );
+
+    const quantity = quantitySelector.value;
 
     if (matchingItem) {
       matchingItem.quantity += 1;
