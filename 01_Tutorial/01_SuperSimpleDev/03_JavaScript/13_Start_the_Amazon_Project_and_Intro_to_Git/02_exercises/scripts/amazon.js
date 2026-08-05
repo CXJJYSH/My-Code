2566066aee143abc49262030ca236765477a3c52
CXJJYSH <1318159468@qq.com>
2026-08-05 14:45:06 +0800

    update

--- 01_Tutorial/01_SuperSimpleDev/03_JavaScript/13_Start_the_Amazon_Project_and_Intro_to_Git/02_exercises/scripts/amazon.js
+++ 01_Tutorial/01_SuperSimpleDev/03_JavaScript/13_Start_the_Amazon_Project_and_Intro_to_Git/02_exercises/scripts/amazon.js
@@ -52,7 +52,40 @@ products.forEach((product) => {
 
 document.querySelector(".js-products-grid").innerHTML = productsHTML;
 
+// We're going to use an object to save the timeout ids.
+// The reason we use an object is because each product
+// will have its own timeoutId. So an object lets us
+// save multiple timeout ids for different products.
+// For example:
+// {
+//   'product-id1': 2,
+//   'product-id2': 5,
+//   ...
+// }
+// (2 and 5 are ids that are returned when we call setTimeout).
+
+// const addedMessageTimeouts = {};
+
 document.querySelectorAll(".js-add-to-cart").forEach((button) => {
+  // This solution uses a feature of JavaScript called a
+  // closure. Each time we run the loop, it will create
+  // a new variable called addedMessageTimeoutId and do
+  // button.addEventListener().
+  //
+  // Then, because of closure, the function we give to
+  // button.addEventListener() will get a unique copy
+  // of the addedMessageTimeoutId variable and it will
+  // keep this copy of the variable forever.
+  // (Reminder: closure = if a function has access to a
+  // value/variable, it will always have access to that
+  // value/variable).
+  //
+  // This allows us to create many unique copies of the
+  // addedMessageTimeoutId variable (one for every time
+  // we run the loop) so it lets us keep track of many
+  // timeoutIds (one for each product).
+  let addedMessageTimeoutId;
+
   button.addEventListener("click", () => {
     const { productId } = button.dataset;
     // Destructuring shortcut
@@ -94,11 +127,23 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
     );
 
     addedMessage.classList.add("added-to-cart-visible");
+
+    // Check if there's a previous timeout for this
+    // product. If there is, we should stop it.
+    // const previousTimeoutId = addedMessageTimeouts[productId];
+
+    if (addedMessageTimeoutId) {
+      clearTimeout(addedMessageTimeoutId);
+    }
+
+    const timeoutId = setTimeout(() => {
+      addedMessage.classList.remove("added-to-cart-visible");
+    }, 2000);
+
+    // Save the timeoutId for this product
+    // so we can stop it later if we need to.
+    addedMessageTimeoutId = timeoutId;
   });
 });
 
-// 2026.08.04 15:03
-
-// 2026.08.04 15:32
-
-// 2026.08.04 16:26
+// 2026.08.05 14:44
