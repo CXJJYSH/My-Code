afacdaae11235b7b23fb6fc63f48901c7e3da435
CXJJYSH <1318159468@qq.com>
2026-09-02 16:14:55 +0800

    14l

--- 01_Tutorial/01_SuperSimpleDev/03_JavaScript/14_Modules/02_exercises/scripts/checkout.js
+++ 01_Tutorial/01_SuperSimpleDev/03_JavaScript/14_Modules/02_exercises/scripts/checkout.js
@@ -1,4 +1,9 @@
-import { cart, removeFromCart, calculateCartQuantity } from "../data/cart.js";
+import {
+  cart,
+  removeFromCart,
+  calculateCartQuantity,
+  updateQuantity,
+} from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
@@ -142,5 +147,7 @@ document.querySelectorAll(".js-save-link").forEach((link) => {
     );
 
     const newQuantity = Number(quantityInput.value);
+
+    updateQuantity(productId, newQuantity);
   });
 });
