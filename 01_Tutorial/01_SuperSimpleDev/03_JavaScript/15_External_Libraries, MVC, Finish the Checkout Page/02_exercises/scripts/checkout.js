c35d5ca8780cc135e4c33da8a61e3ec9c7d1172c
CXJJYSH <1318159468@qq.com>
2026-09-07 11:25:23 +0800

    15j

--- 01_Tutorial/01_SuperSimpleDev/03_JavaScript/15_External_Libraries, MVC, Finish the Checkout Page/02_exercises/scripts/checkout.js
+++ 01_Tutorial/01_SuperSimpleDev/03_JavaScript/15_External_Libraries, MVC, Finish the Checkout Page/02_exercises/scripts/checkout.js
@@ -1,5 +1,11 @@
+import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 
+renderCheckoutHeader();
 renderOrderSummary();
 renderPaymentSummary();
+
+// 2026.09.07 11:17
+
+// 2026.09.07 11:24
