4ae3976911550317529634d49c6a2521b1b15729
CXJJYSH <1318159468@qq.com>
2026-09-07 10:40:02 +0800

    15f

---
+++ 01_Tutorial/01_SuperSimpleDev/03_JavaScript/15_External_Libraries, MVC, Finish the Checkout Page/02_exercises/solutions/202 15f.js
@@ -0,0 +1,9 @@
+function isWeekend(date) {
+  const dayOfWeek = date.format("dddd");
+
+  return dayOfWeek === "Saturday" || dayOfWeek === "Sunday";
+}
+
+export default isWeekend;
+
+// 2026.09.07 10:36
