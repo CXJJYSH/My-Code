d38ecd73ffc772d13b2519e4ba8877d959b7a23e
CXJJYSH <1318159468@qq.com>
2026-09-07 10:59:15 +0800

    15h

--- 01_Tutorial/01_SuperSimpleDev/03_JavaScript/15_External_Libraries, MVC, Finish the Checkout Page/02_exercises/scripts/checkout/orderSummary.js
+++ 01_Tutorial/01_SuperSimpleDev/03_JavaScript/15_External_Libraries, MVC, Finish the Checkout Page/02_exercises/scripts/checkout/orderSummary.js
@@ -116,11 +116,13 @@ export function renderOrderSummary() {
       const productId = link.dataset.productId;
       removeFromCart(productId);
 
-      const container = document.querySelector(
-        `.js-cart-item-container-${productId}`,
-      );
+      // const container = document.querySelector(
+      //   `.js-cart-item-container-${productId}`,
+      // );
 
-      container.remove();
+      // container.remove();
+
+      renderOrderSummary();
 
       updateCartQuantity();
 
@@ -240,3 +242,5 @@ export function renderOrderSummary() {
     });
   });
 }
+
+// 2026.09.07 10:58
