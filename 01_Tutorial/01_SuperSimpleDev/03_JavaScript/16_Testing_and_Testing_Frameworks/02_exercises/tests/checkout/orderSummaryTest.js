3a08be8cebe997ff66dd3b17bdcc3725b7351dea
CXJJYSH <1318159468@qq.com>
2026-09-08 15:19:35 +0800

    16g

--- 01_Tutorial/01_SuperSimpleDev/03_JavaScript/16_Testing_and_Testing_Frameworks/02_exercises/tests/checkout/orderSummaryTest.js
+++ 01_Tutorial/01_SuperSimpleDev/03_JavaScript/16_Testing_and_Testing_Frameworks/02_exercises/tests/checkout/orderSummaryTest.js
@@ -48,6 +48,14 @@ describe("test suite: renderOrderSummary", () => {
     expect(
       document.querySelector(`.js-product-quantity-${productId2}`).innerText,
     ).toContain("Quantity: 1");
+
+    expect(
+      document.querySelector(`.js-product-name-${productId1}`).innerText,
+    ).toEqual("Black and Gray Athletic Cotton Socks - 6 Pairs");
+
+    expect(
+      document.querySelector(`.js-product-name-${productId2}`).innerText,
+    ).toEqual("Intermediate Size Basketball");
   });
 
   it("removes a product", () => {
@@ -63,5 +71,8 @@ describe("test suite: renderOrderSummary", () => {
     ).not.toEqual(null);
     expect(cart.length).toEqual(1);
     expect(cart[0].productId).toEqual(productId2);
+    expect(
+      document.querySelector(`.js-product-name-${productId2}`).innerText,
+    ).toEqual("Intermediate Size Basketball"); // 这句放到两句expect前也可以。
   });
 });
