db98e8ba57db15945fbe6c8c449864b74505a378
CXJJYSH <1318159468@qq.com>
2026-09-08 14:58:29 +0800

    16d

--- 01_Tutorial/01_SuperSimpleDev/03_JavaScript/16_Testing_and_Testing_Frameworks/02_exercises/tests/data/cartTest.js
+++ 01_Tutorial/01_SuperSimpleDev/03_JavaScript/16_Testing_and_Testing_Frameworks/02_exercises/tests/data/cartTest.js
@@ -44,10 +44,25 @@ describe("test suite: addToCart", () => {
 
     addToCart("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
     expect(cart.length).toEqual(1);
+
     expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(localStorage.setItem).toHaveBeenCalledWith(
+      "cart",
+      JSON.stringify([
+        {
+          productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+          quantity: 1,
+          deliveryOptionId: "1",
+        },
+      ]),
+    );
     expect(cart[0].productId).toEqual("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
     expect(cart[0].quantity).toEqual(1);
   });
 });
 
 // 2026.09.07 18:00
+
+// 16d
+
+// 2026.09.08 14:58
