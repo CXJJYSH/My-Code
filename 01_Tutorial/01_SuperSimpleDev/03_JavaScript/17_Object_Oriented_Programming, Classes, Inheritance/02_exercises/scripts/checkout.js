b2852bd8cc838bba4063abcc459eb71322c11ba7
CXJJYSH <1318159468@qq.com>
2026-09-09 21:08:27 +0800

    17a

--- 01_Tutorial/01_SuperSimpleDev/03_JavaScript/17_Object_Oriented_Programming, Classes, Inheritance/02_exercises/scripts/checkout.js
+++ 01_Tutorial/01_SuperSimpleDev/03_JavaScript/17_Object_Oriented_Programming, Classes, Inheritance/02_exercises/scripts/checkout.js
@@ -2,6 +2,7 @@ import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 // import "../data/cartCopy_class.js"; // Run all the code
+import "../data/car.js";
 
 renderCheckoutHeader();
 renderOrderSummary();
