4bc0635bf1121bca6edfc1b8d4b29b4374a42d77
CXJJYSH <1318159468@qq.com>
2026-09-09 20:29:00 +0800

    prepare for exercises

---
+++ 01_Tutorial/01_SuperSimpleDev/03_JavaScript/17_Object_Oriented_Programming, Classes, Inheritance/02_exercises/scripts/checkout.js
@@ -0,0 +1,12 @@
+import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
+import { renderOrderSummary } from "./checkout/orderSummary.js";
+import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+// import "../data/cartCopy_class.js"; // Run all the code
+
+renderCheckoutHeader();
+renderOrderSummary();
+renderPaymentSummary();
+
+// 2026.09.07 11:17
+
+// 2026.09.07 11:24
