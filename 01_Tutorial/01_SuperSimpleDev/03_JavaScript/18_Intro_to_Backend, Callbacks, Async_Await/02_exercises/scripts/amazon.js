1b510e38e1dcd215f11741e8bb759f4f5943f552
CXJJYSH <1318159468@qq.com>
2026-09-16 19:26:11 +0800

    18p

--- 01_Tutorial/01_SuperSimpleDev/03_JavaScript/18_Intro_to_Backend, Callbacks, Async_Await/02_exercises/scripts/amazon.js
+++ 01_Tutorial/01_SuperSimpleDev/03_JavaScript/18_Intro_to_Backend, Callbacks, Async_Await/02_exercises/scripts/amazon.js
@@ -7,7 +7,20 @@ loadProducts(renderProductsGrid);
 function renderProductsGrid() {
   let productsHTML = "";
 
-  products.forEach((product) => {
+  const url = new URL(window.location.href);
+  const search = url.searchParams.get("search");
+
+  let filteredProducts = products;
+
+  // If a search exists in the URL parameters,
+  // filter the products that match the search.
+  if (search) {
+    filteredProducts = products.filter((product) => {
+      return product.name.includes(search);
+    });
+  }
+
+  filteredProducts.forEach((product) => {
     productsHTML += `
     <div class="product-container">
         <div class="product-image-container">
@@ -88,6 +101,11 @@ function renderProductsGrid() {
   }
 
   updateCartQuantity();
+
+  document.querySelector(".js-search-button").addEventListener("click", () => {
+    const search = document.querySelector(".js-search-bar").value;
+    window.location.href = `amazon.html?search=${search}`;
+  });
 
   document.querySelectorAll(".js-add-to-cart").forEach((button) => {
     // This solution uses a feature of JavaScript called a
