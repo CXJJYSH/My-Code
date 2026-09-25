7cba8a2a75a142b0fa7bbe52495500bb5864c4d5
CXJJYSH <1318159468@qq.com>
2026-09-26 00:07:24 +0800

    5i

---
+++ 01_Tutorial/01_SuperSimpleDev/04_React/05_Proper_React_Setup/02_exercises/054_5i/chatbot-project/src/components/ChatMessage.jsx
@@ -0,0 +1,36 @@
+import dayjs from "dayjs";
+import RobotProfileImage from "../assets/robot.png";
+import UserProfileImage from "../assets/profile-1.jpg";
+import "./ChatMessage.css";
+
+export function ChatMessage({ message, sender, time }) {
+  // console.log(props);
+  // props = properties
+  // Props make our component reusable.
+
+  // const { message, sender } = props;
+  // Destructuring
+
+  return (
+    <div
+      className={sender === "user" ? "chat-message-user" : "chat-message-robot"}
+    >
+      {sender === "robot" && (
+        <img src={RobotProfileImage} className="chat-message-profile" />
+      )}
+      <div className="chat-message-text">
+        {message}
+
+        {/* The "time && (" check is optional. Simon added it just to be safe. */}
+        {time && (
+          <div className="chat-message-time">{dayjs(time).format("HH:mm")}</div>
+        )}
+      </div>
+      {sender === "user" && (
+        <img src={UserProfileImage} className="chat-message-profile" />
+      )}
+    </div>
+  );
+}
+
+console.log(UserProfileImage);
